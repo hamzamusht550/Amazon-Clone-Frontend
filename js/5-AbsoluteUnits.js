db20e16dceb21ef6318bbec2d3592dfece9d6244
hamzamusht550 <115695929+hamzamusht550@users.noreply.github.com>
2024-01-11 14:08:03 +0500

    hhcc

---
+++ js/5-AbsoluteUnits.js
@@ -0,0 +1,29 @@
+
+// switch (true) {
+//     case (window.innerWidth < 820):
+//         document.querySelectorAll("header *").forEach(ele => {
+//             ele.style.width = window.getComputedStyle(ele).getPropertyValue("width")
+//             ele.style.height = window.getComputedStyle(ele).getPropertyValue("height")
+//             ele.style.padding = window.getComputedStyle(ele).getPropertyValue("padding")
+//             ele.style.margin = window.getComputedStyle(ele).getPropertyValue("margin")
+//             ele.style.top = window.getComputedStyle(ele).getPropertyValue("top")
+//             ele.style.bottom = window.getComputedStyle(ele).getPropertyValue("bottom")
+//             ele.style.left = window.getComputedStyle(ele).getPropertyValue("left")
+//             ele.style.right = window.getComputedStyle(ele).getPropertyValue("right")
+//             ele.style.fontSize = window.getComputedStyle(ele).getPropertyValue("font-size")
+//         })
+//         document.querySelectorAll("*").forEach(ele => {
+//             ele.style.fontSize = window.getComputedStyle(ele).getPropertyValue("font-size")
+//         }
+//         break;
+// }
+// document.querySelectorAll(".section-4cards *").forEach(ele => {
+//     ele.style.height = window.getComputedStyle(ele).getPropertyValue("height")
+//     ele.style.padding = window.getComputedStyle(ele).getPropertyValue("padding")
+//     ele.style.margin = window.getComputedStyle(ele).getPropertyValue("margin")
+//     ele.style.top = window.getComputedStyle(ele).getPropertyValue("top")
+//     ele.style.bottom = window.getComputedStyle(ele).getPropertyValue("bottom")
+//     ele.style.left = window.getComputedStyle(ele).getPropertyValue("left")
+//     ele.style.right = window.getComputedStyle(ele).getPropertyValue("right")
+//     ele.style.fontSize = window.getComputedStyle(ele).getPropertyValue("font-size")
+// })
